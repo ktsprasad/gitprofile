b61b2cec877e368972a4388521581d523fff711f
ktsprasad <ktsrinivasgowda@gmail.com>
2024-01-07 01:23:09 +0530

    workflow

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -105,11 +105,11 @@ const config = {
     },
   ], */
   // Display blog posts from your medium or dev account. (Optional)
-  /* blog: {
+  blog: {
     source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    username: 'ktsprasad', // to hide blog section, keep it empty
     limit: 2, // How many posts to display. Max is 10.
-  }, */
+  },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
